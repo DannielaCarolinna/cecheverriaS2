19232d2061cfb02974f105c1884737fce0779043
carol <carol@DESKTOP-5DTVH9M>
2023-04-25 01:12:49 -0500

    Agregar archivos de proyecto.

---
+++ cecheverriaS2/cecheverriaS2/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cecheverriaS2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+            if (usuario == "estudiante2023" && contrasena == "uisrael2023")
+            {
+                Navigation.PushAsync(new Operaciones());
+                txtUsuario.Text = "";
+                txtContrasena.Text = "";
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario/Contrasena incorrectos", "Cerrar");
+                txtUsuario.Text = "";
+                txtContrasena.Text = "";
+            }
+        }
+
+        private void btnLimpioar_Clicked(object sender, EventArgs e)
+        {
+            txtUsuario.Text = "";
+            txtContrasena.Text = "";
+        }
+    }
+}
---
+++ cecheverriaS2/cecheverriaS2/Operaciones.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cecheverriaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            //variable double
+            double NotaSeguimiento1 = Convert.ToDouble(txtNotaSeguimiento1.Text);
+            double NotaExamen1 = Convert.ToDouble(txtNotaExamen1.Text);
+            double NotaSeguimiento2 = Convert.ToDouble(txtNotaSeguimiento2.Text);
+            double NotaExamen2 = Convert.ToDouble(txtNotaExamen2.Text);
+            if ((NotaSeguimiento1>=0 && NotaSeguimiento1<=10) && (NotaSeguimiento2 >= 0 && NotaSeguimiento2 <= 10))
+            {
+                double resultado1 = (NotaSeguimiento1 * 0.6) + (NotaExamen1 * 0.4);
+                txtResultado1.Text = resultado1.ToString();
+                double resultado2 = (NotaSeguimiento2 * 0.6) + (NotaExamen2 * 0.4);
+                txtResultado2.Text = resultado2.ToString();
+                double NotaFinal = (NotaSeguimiento1 + NotaSeguimiento2) / 2;
+                txtNotaFinal.Text = NotaFinal.ToString();
+                if (NotaFinal >= 7)
+                {
+                    string estado = "Aprobado";
+                    txtEstado.Text = estado;
+                }
+                else
+                {
+                    string estado = "Examen Complementario";
+                    txtEstado.Text = estado;
+                }
+            }
+            else 
+            {
+                DisplayAlert("Error", "El valor ingresado es incorrecto, favor ingresar valores dentro del rango permitido [0 - 10]", "Cerrar");
+                txtNotaSeguimiento1.Text = "";
+                txtNotaExamen2.Text = "";
+                txtNotaSeguimiento1.Text = "";
+                txtNotaExamen1.Text = "";
+            }
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            txtNotaSeguimiento1.Text = "";
+            txtNotaSeguimiento2.Text = "";
+            txtResultado1.Text = "";
+            txtResultado2.Text = "";
+            txtNotaExamen1.Text = "";
+            txtNotaExamen2.Text = "";
+            txtEstado.Text = "";
+            txtNotaFinal.Text = "";
+        }
+
+        private void btnCerrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+            
+        }
+    }
+}
